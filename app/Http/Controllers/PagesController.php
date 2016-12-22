0493f55c5dce794217062b8d2fb5aca6b737ac19
Neil Sarkar <nsarkar@lorica300.local>
2016-12-22 16:00:04 +1100

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class PagesController extends Controller
+{
+    public function home(){
+		return view('pages.welcome');
+    }
+
+    public function about(){
+    	return view('pages.about');
+    }
+}
---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-md1 col-md-offset-5">
+            <h1>tikTrak</h1>
+            <form>
+                <label>
+                <input type="text" class="form-control" placeholder="Track a ticker" name="ticker">
+                <button type="submit" class="btn">go</button>
+                </label>
+            </form>
+        </div>
+    </div>
+@stop
